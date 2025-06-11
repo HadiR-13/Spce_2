ebe3d7e18218ff720d75677cc0292c292224c944
yazidnasution02@gmail.com <yazidnasution02@gmail.com>
2025-06-11 10:48:01 +0700

    new home page

--- index.php
+++ index.php
@@ -12,6 +12,20 @@
 </head>
 <body>
     <?php include './content/navbar.php'; ?>
+    <main>
+        <div class="hero-content">
+            <div class="text-content">
+                <h2>WANNA TRAVEL TO....</h2>
+                <h1>SPACE?</h1>
+                 <p> When you want to go to space, <br /> you might as well bring back something worth the journey. <br /> Sit down and buckle up for a new experience! </p>
+            </div>
+            <div class="explore-button">
+                <button>
+                    <a href="destination.php">EXPLORE</a>
+                </button>
+            </div>
+        </div>
+    </main>
     <script src="js/script.js"></script>
 </body>
 </html>
